07652e96c34ab5970eed9f9dcd5b3f10d8761a04
egekol <egekol@hotmail.com>
2021-07-13 00:05:55 +0300

    Add project files.

---
+++ RabbitMQ.Receiving/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Receiving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                //Neden burada da queue başlattık? Çünkü bağlantıyı producerdan önce consumer da başlatmak isteyebilir. Mesajlar alınmadan önce queue'nun varlığından emin olunmalı.
+
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "HelloTheFirst",
+                         false,
+                         false,
+                         false,
+                         null);
+
+                    Console.WriteLine(" test");
+                    //servera mesajları queue'dan çekeceğimizi söylemeliyiz. Mesajlar geldiği anda çekmeyeceğimiz için bunu bildirmeliyiz:
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+                    channel.BasicConsume(queue: "hello",
+                        autoAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+
+
+
+                }
+
+
+            }
+        }
+    }
+}
---
+++ Rabbit_Test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Rabbit_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Connection to Rabbit MQ. Write IP if server.
+            var factory = new ConnectionFactory() {HostName = "localhost"} ;
+            using (var connection = factory.CreateConnection())
+            {
+                //We need channel to make a connection.(discord ex.)
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue:"HelloTheFirst",
+                        false, //fiziksel olarak ya da bellekte sakla.
+                        false, //Bulunduğu queue bağlantısı kapandıktan sonra silinir.
+                        false,//Kuyrukta consumer kalmadığında silinir.
+                        null);//Collections.Generic.IDictionary(map (dictionary) of arbitrary key/value pairs)
+
+                    string message = "Hello World!";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("","hello", null, body);
+                    channel.BasicPublish("", "hello", null, Encoding.UTF8.GetBytes("TestOneTwo"));
+                    Console.WriteLine("[x] Sent {0}", message);
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+        }
+    }
+}
